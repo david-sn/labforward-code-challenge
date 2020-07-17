23d0760a3733726a2b4870450604ab8632902ac2
David Shiref <david.sn|@yahoo.com>
2020-07-17 02:45:36 +0200

    add service java docs

--- src/main/java/com/labforward/api/hello/service/HelloWorldService.java
+++ src/main/java/com/labforward/api/hello/service/HelloWorldService.java
@@ -30,10 +30,18 @@ public class HelloWorldService {
         save(getDefault());
     }
 
+    /**
+     * created default greeting
+     * @return default greeting
+     */
     private static Greeting getDefault() {
         return new Greeting(DEFAULT_ID, DEFAULT_MESSAGE);
     }
 
+    /**
+     * @param request the greeting need to be created
+     * @return saved Greeting
+     */
     public Greeting createGreeting(Greeting request) {
         entityValidator.validateCreate(request);
 
@@ -41,19 +49,31 @@ public class HelloWorldService {
         return save(request);
     }
 
+    /**
+     * @param id the greeting need to be retrieved
+     * @return Optional as box maybe Greeting not found for retrieved
+     */
     public Optional<Greeting> getGreeting(String id) {
         Greeting greeting = greetings.get(id);
         if (greeting == null) {
             return Optional.empty();
         }
-
         return Optional.of(greeting);
     }
 
+    /**
+     * @return default greeting
+     */
     public Optional<Greeting> getDefaultGreeting() {
         return getGreeting(DEFAULT_ID);
     }
 
+    /**
+     * update exist greeting in database
+     * @param id need to be updated
+     * @param newGreeting need to be update message
+     * @return Optional as box maybe Greeting not found for updating
+     */
     public Optional<Greeting> updateGreeting(String id, Greeting newGreeting) {
         entityValidator.validateUpdate(newGreeting);
 
@@ -65,16 +85,25 @@ public class HelloWorldService {
             return Optional.empty();
         }
     }
-    
+
+    /**
+     * get all exists greeting in database
+     * @return list All greets
+     */
     public List<Greeting> getAllGreetings() {
         List<Greeting> data = new ArrayList<>(greetings.size());
         if (!greetings.isEmpty()) {
-            this.greetings.entrySet().stream().forEach(greet->data.add(greet.getValue()));
+            this.greetings.entrySet().stream().forEach(greet -> data.add(greet.getValue()));
         }
         return data;
     }
-    
-     public Optional<Greeting> deleteGreeting(String id) {
+
+    /**
+     * deleting service for delete existing greeting
+     * @param id greeting Id
+     * @return Optional as box maybe Greeting not found
+     */
+    public Optional<Greeting> deleteGreeting(String id) {
         Optional<Greeting> existingGreeting = getGreeting(id);
         if (existingGreeting.isPresent()) {
             return Optional.of(delete(existingGreeting.get()));
@@ -83,12 +112,21 @@ public class HelloWorldService {
         }
     }
 
+    /**
+     * internal function for saving new data
+     * @param greeting need to be saved as new
+     * @return saved greeting
+     */
     private Greeting save(Greeting greeting) {
         this.greetings.put(greeting.getId(), greeting);
 
         return greeting;
     }
-    
+
+    /**
+     * @param greeting need to be deleted
+     * @return delete greeting
+     */
     private Greeting delete(Greeting greeting) {
         return this.greetings.remove(greeting.getId());
     }
